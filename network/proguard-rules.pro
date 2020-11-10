049c6da801e6babfd53cf4c806f427c10f55cb43
CoolSnow <coolsnow2020@gmail.com>
2020-11-10 14:05:45 +0800

    set glide https unsafe

--- network/proguard-rules.pro
+++ network/proguard-rules.pro
@@ -10,5 +10,8 @@
 -keep class com.bumptech.glide.load.data.ParcelFileDescriptorRewinder$InternalRewinder {
   *** rewind();
 }
+-keep public class  extends com.bumptech.glide.module.AppGlideModule
+-keep class com.bumptech.glide.GeneratedAppGlideModuleImpl
+-keep class app.tv.network.glide.UnsafeOkHttpGlideModule
 # for DexGuard only
 -keepresourcexmlelements manifest/application/meta-data@value=GlideModule
---
+++ network/src/main/java/app/tv/network/glide/UnsafeOkHttpGlideModule.java
@@ -0,0 +1,30 @@
+package app.tv.network.glide;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.AppGlideModule;
+
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * File: UnsafeOkHttpGlideModule
+ * Author: CoolSnow(coolsnow2020@gmail.com)
+ * Created at: 2020/11/10 10:45
+ * Description:
+ */
+@GlideModule
+public class UnsafeOkHttpGlideModule extends AppGlideModule {
+    @Override
+    public void registerComponents(Context context, Glide glide, Registry registry) {
+        OkHttpClient client = UnsafeOkHttpClient.getUnsafeOkHttpClient();
+        registry.replace(GlideUrl.class, InputStream.class,
+                new OkHttpUrlLoader.Factory(client));
+    }
+}
